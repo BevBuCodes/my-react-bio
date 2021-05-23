e1faf9705b1f911c6ee8232f13223a10321440c7
Beverly <beverlybuscher@gmail.com>
2021-05-22 19:28:34 -0500

    updated about me

---
+++ src/components/AboutMe.js
@@ -0,0 +1,7 @@
+// This is a functional component
+
+function AboutMe(props) {
+    return <h2>Hi, I'm {props.name}</h2>
+}
+
+export default AboutMe;
---
+++ src/components/AboutMeClass.js
@@ -0,0 +1,12 @@
+// This is a class component
+
+import React from 'react';
+
+class AboutMeClass extends React.Component{
+
+    render() {
+        return <h3>I am learning React and LOVE it!</h3>
+    }
+}
+
+export default AboutMeClass;
